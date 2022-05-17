2bc16f304d3ea837b9bd4509c0255fe4f9adf8fb
AinRuthPai <tanbing@naver.com>
2022-05-17 22:48:53 +0900

    2번 기능까지 구현 완료(GamePage.jsx 주석 참고)

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,24 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 function GamePage() {
   const [ready, setReady] = useState(true);
 
+  const startTime = useRef();
+  const endTime = useRef();
+
+  // 1. 클릭 화면이 랜덤한 시간에 생성 (2~5초)
+  useEffect(() => {
+    if (ready === true) {
+      startTime.current = new Date();
+      setTimeout(() => {
+        setReady(false);
+      }, Math.floor((Math.random() * 4 + 2) * 1000));
+    }
+  });
+
+  // 2. 대기 화면에서 클릭 시 경고 화면으로 넘어가고, 클릭 시 다시 대기화면으로 복귀
+  // 일단 alert으로 해결
   function onClickReady() {
+    if (ready === true) {
+      alert("파란색이 나타나면 눌러주세요");
+    }
+    endTime.current = new Date();
     setReady(true);
   }
 
-  // function randomTime() {
-  //   Math.floor(Math.random() * 4) + 2;
-  // }
-  // let sec = randomTime();
-
-  useEffect(() => {
-    let timer = setTimeout(() => {
-      setReady(false);
-      console.log("run!");
-    }, 3000);
-  });
+  // 3. 정상적으로 클릭화면에서 클릭 시 반응 속도(ms) 측정
 
   return (
     <form>
