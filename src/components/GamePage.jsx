401783bbbea5eefaf54e4ddc90ca80aec5eeb9cb
AinRuthPai <tanbing@naver.com>
2022-05-18 00:50:33 +0900

    완성

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 function GamePage() {
   const [ready, setReady] = useState(true);
+  const [result, setResult] = useState([]);
 
   const startTime = useRef();
   const endTime = useRef();
@@ -10,41 +11,47 @@ function GamePage() {
   // 1. 클릭 화면이 랜덤한 시간에 생성 (2~5초)
   useEffect(() => {
     if (ready === true) {
-      startTime.current = new Date();
       setTimeout(() => {
         setReady(false);
+        startTime.current = new Date();
       }, Math.floor((Math.random() * 4 + 2) * 1000));
     }
   });
 
   // 2. 대기 화면에서 클릭 시 경고 화면으로 넘어가고, 클릭 시 다시 대기화면으로 복귀
   // 일단 alert으로 해결
+
+  // 3. 정상적으로 클릭화면에서 클릭 시 반응 속도(ms) 측정
+  // useRef, new Date() 이용해서 setReady가 false가 된 순간과 클릭이 된 순간의 시간을 따로 가져와서 두 순간의 시간을 빼서 계산하여 결과 도출
   function onClickReady() {
     if (ready === true) {
       alert("파란색이 나타나면 눌러주세요");
     }
     endTime.current = new Date();
     setReady(true);
+    setResult([endTime.current - startTime.current]);
   }
 
-  // 3. 정상적으로 클릭화면에서 클릭 시 반응 속도(ms) 측정
-
   return (
-    <form>
-      <section className='game-section'>
-        <div onClick={onClickReady} className={ready ? "circle" : "circle blueCircle"}>
-          <p className={ready ? "ready" : "click"}>{ready ? "ready..." : "click!"}</p>
-          <span>파란색으로 바뀌면 클릭해주세요</span>
-        </div>
-        <p className='time'>반응시간 : xxx ms</p>
-        <div>
-          <button className='reset'>다시하기</button>
-          <Link to='/'>
-            <button className='back'>돌아가기</button>
-          </Link>
-        </div>
-      </section>
-    </form>
+    <section className='game-section'>
+      <div onClick={onClickReady} className={ready ? "circle" : "circle blueCircle"}>
+        <p className={ready ? "ready" : "click"}>{ready ? "ready..." : "click!"}</p>
+        <span>파란색으로 바뀌면 클릭해주세요</span>
+      </div>
+      <p className='time'>반응시간 : {result} ms</p>
+      <div>
+        <button
+          className='reset'
+          onClick={() => {
+            window.location.reload();
+          }}>
+          다시하기
+        </button>
+        <Link to='/'>
+          <button className='back'>돌아가기</button>
+        </Link>
+      </div>
+    </section>
   );
 }
 
